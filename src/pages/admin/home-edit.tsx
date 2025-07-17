d5d209f41a1ae3eb2346d0541365b9d018d75200
Marko Milinkovic <markomilinkovic10@gmail.com>
2025-07-17 12:49:27 +0200

    Reset projekta, počinjemo iz početka

--- src/pages/admin/home-edit.tsx
+++
@@ -1,8 +0,0 @@
-const res = await fetch('/api/admin/home', {
-  method: 'PUT',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`, // token iz localStorage, cookie, context itd.
-  },
-  body: JSON.stringify(homeData),
-});
